18dee481f53d85748ff9337d875bd7ab1142b69a
Siva-Komma <kommasivajyothi22@gmail.com>
2023-04-23 21:45:02 +0530

    Operations on ArrayList

---
+++ NLogDemo/ArrayListCollection.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLogDemo
+{
+    class ArrayListCollection
+    {
+        public static void Collection()
+        {
+            ArrayList list = new ArrayList();
+            list.Add("Hi");
+            list.Add(1);
+            list.Add(2);
+            list.Add("HWR");
+            list.Add(3);
+            foreach(var data in list)
+            {
+                Console.WriteLine("Printing array list elements"+data);
+            }
+            Console.WriteLine("\n");
+            list.Insert(3, 10);
+            foreach (var data in list)
+            {
+                Console.WriteLine("Printing after inserting array elemen" + data);
+            }
+            Console.WriteLine("\n");
+            list.Remove(10);
+            foreach (var data in list)
+            {
+                Console.WriteLine("Printing after deleting array elemen" + data);
+            }
+        }
+    }
+}
--- NLogDemo/Program.cs
+++ NLogDemo/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +14,7 @@ namespace NLogDemo
             Console.WriteLine("Hello, World!");
             AddNumbers num = new AddNumbers();
             num.Sum(10, 20);
+            ArrayListCollection.Collection();
             Console.ReadLine();
         }
     }
